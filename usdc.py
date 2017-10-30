755b6dfde9df77bcab4960366502e256fb195e03
= <jc.ismostlikely@gmail.com>
2017-10-30 06:55:22 -0400

    Error prevention

--- usdc.py
+++ usdc.py
@@ -2,5 +2,11 @@ def usd_to_egp(usd):
     amount = usd * 17.62
     print('That is about', amount, 'EGP')
 
-while True:
-    usd_to_egp(int(input("USD: ")))
+
+if __name__ == "__main__":
+    while True:
+        usd = input("USD: ")
+        if usd.isdigit():
+            usd_to_egp(usd)
+        else:
+            print("Invalid input")
